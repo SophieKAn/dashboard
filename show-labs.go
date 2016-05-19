9ceeffb733f58f689d93a0a44c068ba8eb3c9f48
Sophie <sophieanderson80@gmail.com>
2016-05-19 10:27:21 -0700

    addes a config.json file from which the js file and the go program will read

--- show-labs.go
+++ show-labs.go
@@ -1,70 +1,31 @@
-//Sophia Anderson | ander569@wwu.edu
-//The goal of this program is to access all the computers in the labs and
-//indicate whether each one is running Windows or Linux, or is inaccessible.
+/* Sophia Anderson | ander569@wwu.edu */
+// The goal of this program is to access all the computers in the labs and
+// indicate whether each one is running Windows or Linux, or is inaccessible.
 
 package main
 
 import(
-  "fmt"
   "net"
+  "fmt"
   "time"
   "color"
+  //"io/ioutil"
+  //"encoding/json"
 )
 
 func main() {
-  /*fmt.Println(";;;;;;;;;;;;;;;;;;;;;;;;;CF 162;;;;;;;;;;;;;;;;;;;;;;;;;;;;")
-  for i := 2; i < 28; i++ {
-    ip := fmt.Sprintf("***REMOVED***.137.%v", i)
-    fmt.Println(operatingSystem(ip))
-  }
-
-  fmt.Println(";;;;;;;;;;;;;;;;;;;;;;;;;CF 164;;;;;;;;;;;;;;;;;;;;;;;;;;;;")
-  for i := 35; i < 62; i++ {
-    ip := fmt.Sprintf("***REMOVED***.137.%v", i)
-    fmt.Println(operatingSystem(ip))
-  }
-
-  fmt.Println(";;;;;;;;;;;;;;;;;;;;;;;;;CF 405;;;;;;;;;;;;;;;;;;;;;;;;;;;;")
-  for i := 66; i < 92; i++ {
-    ip := fmt.Sprintf("***REMOVED***.137.%v", i)
-    fmt.Println(operatingSystem(ip))
-  }
-
-  fmt.Println(";;;;;;;;;;;;;;;;;;;;;;;;;CF 414;;;;;;;;;;;;;;;;;;;;;;;;;;;;")
-  for i := 99; i < 120; i++ {
-    ip := fmt.Sprintf("***REMOVED***.137.%v", i)
-    fmt.Println(operatingSystem(ip))
-  }
-
-  fmt.Println(";;;;;;;;;;;;;;;;;;;;;;;;;CF 416;;;;;;;;;;;;;;;;;;;;;;;;;;;;")
-  for i := 130; i < 152; i++ {
-    ip := fmt.Sprintf("***REMOVED***.137.%v", i)
-    fmt.Println(operatingSystem(ip))
-  }
-
-  fmt.Println(";;;;;;;;;;;;;;;;;;;;;;;;;CF 418;;;;;;;;;;;;;;;;;;;;;;;;;;;;")
-  for i := 163; i < 183; i++ {
-    ip := fmt.Sprintf("***REMOVED***.137.%v", i)
-    fmt.Println(operatingSystem(ip))
-  }*/
-
-  fmt.Println(";;;;;;;;;;;;;;;;;;;;;;;;;CF 418;;;;;;;;;;;;;;;;;;;;;;;;;;;;")
   for i := 98; i < 110; i++ {
     ip := fmt.Sprintf("***REMOVED***.30.%v", i)
     fmt.Println(operatingSystem(ip))
   }
-
-
-
-
 }
 
-//Name:operatingSystem
-//Inputs:An IP address in String form
-//Outputs:A string indicating the status of the machine
-//Function:Given the machine's IP address, this function will
-//return a string containing the current OS of that particular
-//machine, or if it is inaccessible at the time.
+/* OPERATING_SYSTEM */
+// Inputs:An IP address in String form
+// Outputs:A string indicating the status of the machine
+// Function:Given the machine's IP address, this function will
+//          return a string containing the current OS of that particular
+//          machine, or if it is inaccessible at the time.
 func operatingSystem(IP string) (string) {
   //look up address
   addr, err := net.LookupAddr(IP)
@@ -84,11 +45,11 @@ func operatingSystem(IP string) (string) {
   }
 }
 
-//Name:tryToConnect
-//Inputs:Two strings: an IP address and a port number
-//Outputs:An error, either nil or not
-//Function:Tries to connect to the given machine using the given port.
-//returns a new error upon failure, and a nil error upon success.
+/* TRY_TO_CONNECT */
+// Inputs:Two strings: an IP address and a port number
+// Outputs:An error, either nil or not
+// Function:Tries to connect to the given machine using the given port.
+//          returns a new error upon failure, and a nil error upon success.
 func tryToConnect(IP string, port string) (error) {
   conn, err := net.DialTimeout("tcp", IP + ":" + port, time.Millisecond*50)
   if err == nil {
