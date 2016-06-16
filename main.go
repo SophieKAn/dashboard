2b9f9afd46a9a47b476a594a77ca8770bb8bf05a
Sophie <sophieanderson80@gmail.com>
2016-06-16 10:19:38 -0700

    processAllLabs now returns the correct data structure

---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import(
+  "os"
+  "fmt"
+  "io/ioutil"
+  "encoding/json")
+
+// main opens and reads config.json into a MyJson type, and then proceeds to
+// find the machine statuses for all the labs outlined in the config file.
+func main() {
+ 
+  labs := getConfig("config.json")
+
+  processAllLabs(labs)
+}
+
+
+
+
+
+
+
+
+/* FUNCTIONS */
+
+// processAllLabs obtains the beginning statuses of all the labs.
+func processAllLabs(labs []interface{}) {
+
+  all_labs := make(map[string][]map[string]int)
+
+  for lab := range labs {
+    prefix,one_lab := processLab(labs[lab].(map[string]interface{}))
+    all_labs[prefix] = one_lab
+  }
+}
+
+// processLab takes the information for one lab and returns an ordered list
+// containing the operating statuses for each machine.
+func processLab(lab map[string]interface{}) (string, []map[string]int) {
+
+  prefix := lab["prefix"]
+  start := int(lab["start"].(float64))
+  end := int(lab["end"].(float64))
+
+  var machines_in_lab []map[string]int
+  machines_in_lab = make([]map[string]int, 1)
+
+  for i := start; i <= end; i++ {
+    hostname := fmt.Sprintf("%s-%02d.***REMOVED***", prefix, i)
+    machine := make(map[string]int)
+    machine["machine"] = i
+    machine["status"] = operatingSystem(hostname)
+    machines_in_lab = append(machines_in_lab, machine)
+  }
+  return prefix.(string), machines_in_lab
+}
+
+// check takes an error. If the error exists it gets printed, and then the
+// program exits. If it doesn't exist, nothing happens.
+func check(e error) {
+  if e != nil {
+    fmt.Printf("Error: %v", e)
+    os.Exit(1)
+  }
+}
+
+// getConfig takes the name of the configuration file (currently "config.json")
+// attempts to open/read file then unmarshal it into a list of interfaces.
+func getConfig(file_name string) []interface{} {
+  config_file, err := ioutil.ReadFile(file_name)
+  check(err)
+
+  var labs []interface{}
+  err = json.Unmarshal(config_file, &labs)
+  check(err)
+  return labs
+}
---
+++ status.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net"
+	"time")
+
+// operatingSystem takes a hostname(str) and returns what operating system that
+// machine is running based on which port is successfully used to connect. 
+// It returns 'linux', 'windows', or 'inaccessible'.
+func operatingSystem(hostname string) (int) {
+  //try to connect on various ports
+  if accessible(hostname, "***REMOVED***") {
+    return 0 
+  } else if accessible(hostname, "***REMOVED***") {
+    return 1 
+  } else {
+    return 2
+  }
+}
+
+
+// tryToConnect takes a hostname and a port number and tries to establish a
+// connection using those parameters.
+// It returns an error, if one occurrs.
+func accessible(hostn string, port string) (bool) {
+  var ok bool
+  conn, err := net.DialTimeout("tcp", hostn + ":" + port, time.Millisecond*50)
+  if err == nil {
+    conn.Close()
+    ok = true
+  } else {
+    ok = false
+  }
+  return ok
+}
+
