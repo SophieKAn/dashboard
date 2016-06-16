2d583ffc692ea30a09bdf51730596f9be680010c
Sophie <sophieanderson80@gmail.com>
2016-06-16 13:34:07 -0700

    beginning to implement server side

--- main.go
+++ main.go
@@ -9,10 +9,13 @@ import(
 // main opens and reads config.json into a MyJson type, and then proceeds to
 // find the machine statuses for all the labs outlined in the config file.
 func main() {
- 
-  labs := getConfig("config.json")
+  labs := getConfig("config.json") //information directly from the config file.
+  lab_status := processAllLabs(labs) //return the statuses in format to send.
 
-  processAllLabs(labs)
+  serve(lab_status)
+
+
+
 }
 
 
@@ -25,32 +28,29 @@ func main() {
 /* FUNCTIONS */
 
 // processAllLabs obtains the beginning statuses of all the labs.
-func processAllLabs(labs []interface{}) {
-
+func processAllLabs(labs []interface{}) (map[string][]map[string]int) {
   all_labs := make(map[string][]map[string]int)
 
   for lab := range labs {
     prefix,one_lab := processLab(labs[lab].(map[string]interface{}))
     all_labs[prefix] = one_lab
   }
+  return all_labs
 }
 
 // processLab takes the information for one lab and returns an ordered list
 // containing the operating statuses for each machine.
 func processLab(lab map[string]interface{}) (string, []map[string]int) {
-
   prefix := lab["prefix"]
   start := int(lab["start"].(float64))
   end := int(lab["end"].(float64))
-
-  var machines_in_lab []map[string]int
-  machines_in_lab = make([]map[string]int, 1)
+  machines_in_lab := make([]map[string]int, 1)
 
   for i := start; i <= end; i++ {
     hostname := fmt.Sprintf("%s-%02d.***REMOVED***", prefix, i)
+
     machine := make(map[string]int)
-    machine["machine"] = i
-    machine["status"] = operatingSystem(hostname)
+    machine["machine"], machine["status"] = i, operatingSystem(hostname)
     machines_in_lab = append(machines_in_lab, machine)
   }
   return prefix.(string), machines_in_lab
--- web.go
+++ web.go
@@ -1,7 +1,20 @@
 package main
 
 import (
+  "fmt"
+  "io/ioutil"
+  "net/http"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+  html_file, err := ioutil.ReadFile("test.html")
+  check(err)
+  fmt.Fprintln(w, string(html_file))
+}
 
+
+func serve(lab_status map[string][]map[string]int) {
+  http.HandleFunc("/initial", handler)
+  http.ListenAndServe(":8080",nil)
+}
 
