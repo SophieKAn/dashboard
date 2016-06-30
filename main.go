ad7425115195b6013efc98a62c1b1eac9f72db47
Sophie <sophieanderson80@gmail.com>
2016-06-30 13:37:41 -0700

    ran go fmt.

--- main.go
+++ main.go
@@ -17,16 +17,16 @@ type Machine struct {
 }
 
 func main() {
-	/* > Start the server and set up framework of labs from config.json */
+	/* > Start the server in its own Goroutine. */
 	go http.ListenAndServe(":8080", http.FileServer(http.Dir("./static")))
 
 	/* > Get lab configuration from config file. */
 	labs := getConfig("./static/config.json")
 
-	/* > Create a struct for each machines */
+	/* > Create a struct for each machine. */
 	all_machines := getMachines(labs)
 
-	/* > Update the statuses continually */
+	/* > Update the statuses every 5 minutes. */
 	for {
 		updateStatuses(all_machines)
 		time.Sleep(5 * time.Minute)
@@ -38,7 +38,7 @@ func main() {
 // pointers to Machine structs representing all the machines in all the labs.
 func getMachines(labs []interface{}) []*Machine {
 	all_machines := make([]*Machine, 1)
-	
+
 	for lab := range labs {
 		this_lab := labs[lab].(map[string]interface{})
 		prefix := this_lab["prefix"].(string)
@@ -47,9 +47,8 @@ func getMachines(labs []interface{}) []*Machine {
 
 		for i := start; i <= end; i++ {
 			hostname := fmt.Sprintf("%s-%02d.***REMOVED***", prefix, i)
-			all_machines = append(all_machines, &Machine{hostname, 1})
+			all_machines = append(all_machines, &Machine{hostname, 2})
 		}
 	}
-
 	return all_machines
 }
