beda800402290cff18b97b0335b88007d1303e4b
Sophie <sophieanderson80@gmail.com>
2016-07-07 12:18:47 -0700

    GetMachines is more appropriate in utils.go

--- utils.go
+++ utils.go
@@ -32,3 +32,22 @@ func GetConfig(fileName string) []interface{} {
 	Check(err)
 	return labs
 }
+
+// getMachines takes the unmarshalled config.json and constructs a slice of
+// pointers to Machine structs representing all the machines indicated in the
+// config.
+func GetMachines(labs []interface{}) []*Machine {
+	allMachines := make([]*Machine, 0)
+
+	for lab := range labs {
+		aLab := labs[lab].(map[string]interface{})
+		prefix := aLab["prefix"].(string)
+		start, end := int(aLab["start"].(float64)), int(aLab["end"].(float64))
+
+		for i := start; i <= end; i++ {
+			hostname := fmt.Sprintf("%s-%02d.***REMOVED***", prefix, i)
+			allMachines = append(allMachines, &Machine{hostname, 2})
+		}
+	}
+	return allMachines
+} 
