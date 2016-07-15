a2df9eece30a286285e7bbfff3d2fe6b9f9a3500
Sophie <sophieanderson80@gmail.com>
2016-07-15 15:05:24 -0700

    Create hub to manage connections

---
+++ hub.go
@@ -0,0 +1,40 @@
+// Copyright 2013 The Gorilla WebSocket Authors.
+
+package main
+
+type hub struct {
+	connections map[*Conn]bool
+	broadcast chan []byte
+	register chan *Conn
+	unregister chan *Conn
+}
+
+var hub = Hub {
+	broadcast:   make(chan []byte),
+	register:    make(chan *Conn),
+	unregister:  make(chan *Conn),
+	connections: make(map[*Conn]bool),
+}
+
+func (h *Hub) run() {
+	for {
+		select {
+		case conn := <-h.register:
+			h.connections[conn] = true
+		case conn := <-h.unregister:
+			if _, ok := h.connections[conn]; ok {
+				delete(h.connections, conn)
+				close(conn.send)
+			}
+		case message := <-h.broadcast:
+			for conn := range h.connections {
+				select {
+				case conn.send <-message:
+				default:
+					close(conn.send)
+					delete(h.connections, conn)
+				}
+			}
+		}
+	}
+}
