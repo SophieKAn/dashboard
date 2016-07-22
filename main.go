9fb51eb85513ad14bfafff9086def9a26ba9b4f3
Sophie <sophieanderson80@gmail.com>
2016-07-22 11:12:50 -0700

    Moved non-command-line argument things to server.go

--- main.go
+++ main.go
@@ -5,10 +5,6 @@ package main
 /////////////
 
 import (
-	"encoding/json"
-	"fmt"
-	"net/http"
-	"time"
 	"github.com/docopt/docopt-go"
 )
 
@@ -30,59 +26,7 @@ type Machine struct {
 }
 
 func main() {
-	args, err := docopt.Parse(usage, nil, true, Version, false)
-	if err != nil {
-		fmt.Println(err)
-	}
-  fmt.Println(len(args))
+	_, _ = docopt.Parse(usage, nil, true, Version, false)
 
-	/* > Get lab configuration */
-	labConfig := GetConfig("./static/config.json")
-	allMachines := GetMachines(labConfig)
-
-	/* > Run the Hub */
-	hub := newHub()
-	go hub.run()
-
-	/* > Start the server */
-	http.Handle("/", http.FileServer(http.Dir("./static")))
-	http.HandleFunc("/upd", func(w http.ResponseWriter, r *http.Request) {
-		ServeUpdates(hub, allMachines, w, r)
-	})
-
-	go http.ListenAndServe("localhost:8080", nil)
-
-	/* > Update forever */
-	var updates []*Machine
-	for {
-		for machine := range UpdateStatuses(allMachines) {
-			updates = append(updates, machine)
-		}
-
-		if updates != nil {
-			message, err := json.Marshal(updates)
-			Check(err)
-			hub.broadcast <- message
-			updates = nil
-		} else {
-			fmt.Println("no changes")
-		}
-
-		time.Sleep(5 * time.Second)
-	}
-}
-
-func ServeUpdates(hub *Hub, allMachines []*Machine, w http.ResponseWriter, r *http.Request) {
-	/* > Open the websocket connection. */
-	ws, err := upgrader.Upgrade(w, r, nil)
-	Check(err)
-	defer ws.Close()
-
-	client := &Client{hub, ws, make(chan []byte)}
-	hub.register <- client
-	go func() {
-		data, _ := json.Marshal(allMachines)
-		client.send <- data
-	}()
-	client.writePump()
+	Server()
 }
---
+++ server.go
@@ -0,0 +1,65 @@
+package main
+
+///////////////
+// Server.go //
+///////////////
+
+import (
+	"time"
+	"fmt"
+	"encoding/json"
+	"net/http"
+)
+
+func Server() {
+
+	/* > Get lab configuration */
+	labConfig := GetConfig("./static/config.json")
+	allMachines := GetMachines(labConfig)
+
+	/* > Run the Hub */
+	hub := newHub()
+	hub.run()
+
+	/* > Start the server */
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.HandleFunc("/upd", func(w http.ResponseWriter, r *http.Request) {
+		ServeUpdates(hub, allMachines, w, r)
+	})
+
+	go http.ListenAndServe("localhost:8080", nil)
+
+	/* > Update forever */
+	var updates []*Machine
+	for {
+		for machine := range UpdateStatuses(allMachines) {
+			updates = append(updates, machine)
+		}
+
+		if updates != nil {
+			message, err := json.Marshal(updates)
+			Check(err)
+			hub.broadcast <- message
+			updates = nil
+		} else {
+			fmt.Println("no changes")
+		}
+
+		time.Sleep(5 * time.Second)
+	}
+}
+
+func ServeUpdates(hub *Hub, allMachines []*Machine, w http.ResponseWriter, r *http.Request) {
+	/* > Open the websocket connections. */
+	ws, err := upgrader.Upgrade(w, r, nil)
+	Check(err)
+	defer ws.Close()
+
+	client := &Client{hub, ws, make(chan []byte)}
+	hub.register <- client
+	go func() {
+		data, _ := json.Marshal(allMachines)
+		client.send <- data
+	}()
+	client.writePump()
+}
