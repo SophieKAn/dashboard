33d258ea1ed548104a866b259cb2166ff0474a82
Sophie <sophieanderson80@gmail.com>
2016-08-01 09:32:50 -0700

    Check error when starting server

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ package main
 /////////////
 
 import (
+	"fmt"
 	"github.com/docopt/docopt-go"
 	"regexp"
 	"strings"
@@ -38,6 +39,7 @@ func main() {
 	interf, port := bindCommand(args["--bind"])
 	debug = args["--debug"].(bool)
 
+	PrintStuff(interf, port, config, debug)
 	Server(interf, port, config, debug)
 }
 
@@ -77,6 +79,8 @@ func bindCommand(input interface{}) (string, string) {
 }
 
 func mapSubexpNames(m, n []string) map[string]string {
+	/* http://stackoverflow.com/a/30483899/6279238 */
+	/* Code found in comment on main answer */
 	m, n = m[1:], n[1:]
 	r := make(map[string]string, len(m))
 	for i, _ := range n {
@@ -85,10 +89,10 @@ func mapSubexpNames(m, n []string) map[string]string {
 	return r
 }
 
-//func PrintStuff(intf string, port string, config string, debug bool) {
-//fmt.Printf("Interface: %s\n", intf)
-//fmt.Printf("Port:      %s\n", port)
-//fmt.Printf("Config:    %s\n", config)
-//fmt.Printf("Debug:     %t\n", debug)
-//fmt.Println()
-//}
+func PrintStuff(intf string, port string, config string, debug bool) {
+	fmt.Printf("Interface: %s\n", intf)
+	fmt.Printf("Port:      %s\n", port)
+	fmt.Printf("Config:    %s\n", config)
+	fmt.Printf("Debug:     %t\n", debug)
+	fmt.Println()
+}
