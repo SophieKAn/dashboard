948ea913340675a9eef2f4105bd7c25168c04905
Sophie <sophieanderson80@gmail.com>
2016-08-10 16:25:37 -0700

    Use .map function to improve solution

--- static/js/script.js
+++ static/js/script.js
@@ -3,12 +3,12 @@ function initializePage(url) {
   req.onreadystatechange = () => {
     if (req.readyState === XMLHttpRequest.DONE) {
       if (req.status === 200) {
-        let json = JSON.parse(req.responseText);
+        let result = JSON.parse(req.responseText);
         let body = document.getElementsByTagName('body')[0];
         let widget = document.createElement('ul');
         widget.className = "widget";
         body.appendChild(widget);
-        json["machineRanges"].forEach(l => {
+        result.machineRanges.forEach(l => {
           let cs_lab = document.createElement('ul');
           cs_lab.className = "cs_lab";
           let lab_title = document.createElement('header');
@@ -20,7 +20,7 @@ function initializePage(url) {
             createLabMachine(cs_lab, i);
           }
         });
-        updater(json["interface"], json["port"], json["machineIdentifiers"]);
+        updater(result);
       }
     }
   }
@@ -38,15 +38,15 @@ function createLabMachine(cs_lab, i) {
 
 
 
-function updater(interf, port, machineIdentifiers) {
-  var conn = new WebSocket("ws://" + interf + ":" + port + "/upd");
+function updater(result) {
+  var conn = new WebSocket("ws://" + result.interface + ":" + result.port + "/upd");
   conn.onclose = function(evt) {
     console.log("Connection closed");
   }
   conn.onmessage = function(evt) {
     let machineData = JSON.parse(evt.data);
     console.log("updating");
-    changeStatus(machineData, machineIdentifiers);
+    changeStatus(machineData, result.machineIdentifiers);
   }
 }
 
@@ -59,11 +59,7 @@ function changeStatus(machineData, machineIdentifiers) {
         el.style.background = s.color;
       }
     });
-	  var idArray = [];
-	  for (var i in machineIdentifiers) {
-      idArray.push(machineIdentifiers[i].name);
-    }
-    if (!isInArray(m.status, idArray)) {
+		if (!machineIdentifiers.map(p => p.name).includes(m.status)) {
 			el.style.background = "gray";
 		}
   });
@@ -73,8 +69,4 @@ function pad(n) {
   // http://stackoverflow.com/a/8089938/6279238
   // only for positive integers
   return (n < 10) ? ("0" + n.toString()) : String(n);
-}
-
-function isInArray(value, array) {
-  return array.indexOf(value) > -1;
 }
