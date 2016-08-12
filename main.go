295cbee52eafc7cc5f15084266c60e382c17b7b9
Sophie <sophieanderson80@gmail.com>
2016-08-12 09:17:05 -0700

    Simplify main, change names for clarity

--- main.go
+++ main.go
@@ -4,26 +4,25 @@ package main
 // Main.go //
 /////////////
 
-const (
-	Usage = `Start a web server to display current usage of labs.
+
+/*
+Usage: Start a web server to display current usage of labs.
 
 Usage:
-  dashboard [options]
-  dashboard --version
-  dashboard -h | --help
+	dashboard [options]
+	dashboard --version
+	dashboard -h | --help
 
 Options:
-  --debug                                             Turn on debugging output.
+	--debug
   -b, --bind=(<interface>:<port>|<interface>|:<port>) Set the interface and port for the server.
   -c, --config=<file>                                 Specify a configuration file.
   -i, --interval=(<sec>s|<min>m|<hr>h)`
-
-	Version = "dashboard 1.0.0"
-)
+*/
 
 func main() {
-	var configs Config
-	configs.Configure()
+	var settings Config
+	settings.Configure()
 
-	RunServer(configs)
+	RunServer(settings)
 }
