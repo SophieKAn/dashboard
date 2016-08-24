c63d657f8ba847f26500891d138eacc89a34abe7
Sophie <sophieanderson80@gmail.com>
2016-08-24 13:30:28 -0700

    Restructure and add support files

--- main.go
+++ main.go
@@ -1,24 +1,5 @@
 package main
 
-/////////////
-// Main.go //
-/////////////
-
-/*
-Usage: Start a web server to display current usage of labs.
-
-Usage:
-	dashboard [options]
-	dashboard --version
-	dashboard -h | --help
-
-Options:
-	--debug
-  -b, --bind=(<interface>:<port>|<interface>|:<port>) Set the interface and port for the server.
-  -c, --config=<file>                                 Specify a configuration file.
-  -i, --interval=(<sec>s|<min>m|<hr>h)`
-*/
-
 func main() {
 	settings := new(Config)
 	settings.Configure()
